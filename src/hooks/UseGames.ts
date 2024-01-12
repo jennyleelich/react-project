3a302563bff19fe760a45528b0007af8e1a39cd1
chunhua li <chunhuali@chunhuas-iMac.hsd1.nj.comcast.net>
2024-01-12 10:44:23 -0500

    fix gamecard error

--- src/hooks/UseGames.ts
+++ src/hooks/UseGames.ts
@@ -18,7 +18,7 @@ export interface Game {
 }
 
 
-const useGames = (gameQuery:GameQuery) => useData<GameQuery>('/games', {
+const useGames = (gameQuery:GameQuery) => useData<Game>('/games', {
     params: {
         genres: gameQuery.genre?.id, 
         platforms: gameQuery.platform?.id,
